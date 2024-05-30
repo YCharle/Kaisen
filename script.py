da574bc62514a398df9002d68d7e659fdfa7295c
YCharle <yjdc2002@gmail.com>
2024-05-30 18:27:51 -0400

    Add Questions

--- script.py
+++ script.py
@@ -3,8 +3,13 @@ class Player:
     def __init__(self, name, hp = 100):
         self.name = name
         self.hp = hp
+
     def __repr__(self):
         return "Welcome to Academia Kaisen {name}! You have {health} health points.".format(name=self.name, health=self.hp)
+    
+    def gain_points(self):
+        pass
+    
     
 player_1 = input("Player 1, input your name: ")
 player_2 = input("Player 2, input your name: ")
@@ -26,7 +31,7 @@ else:
     print("We need players to continue.")
 
 
-
+ks3_questions = {"Which of the following is not in an animal cell?\nCell Membrane\nCell Wall\nCytoplasm\nNucleus": "Cell wall", "Which of the folowing about veins is not true?\nSubstances cannot pass through their walls\nThey carry blood to the heart\nThey have no valves\nThey have thin walls": "They have no valves", "How many faces does a square-based pyramid have?": 5, "The earth is made up of how many layers?": 5, "What is the capital of Brazil?": "Brasilia", "Spell Synagogue": "Synagogue", "Spell Isosceles": "Isosceles", "What is the collective noun for a group of crows?": "Murder", "What is the plural of chief?": "Chiefs", "What is 15 times 17?": 255}
 #print(list_of_players)
 #print(p1)
 #print(p2)
